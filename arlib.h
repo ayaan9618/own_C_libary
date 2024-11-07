78d03d71b74283d2248de935b8b617d42862e9f4
Mohammed Ayaan ur Rahman <135800053+ayaan9618@users.noreply.github.com>
2024-11-07 12:26:19 +0530

    added .h file

---
+++ arlib.h
@@ -0,0 +1,28 @@
+#ifndef ARLIB_H
+#define ARLIB_H
+
+double sqr(double i);
+
+
+double cube(double i );
+
+double add( double i, double j );
+
+double sub( double i, double j );
+
+double mul( double i, double j );
+
+double divi( double i, double j );
+
+int mod( int i, int j );
+
+int takeint(const char *prompt);
+
+float takefloat(const char *prompt);
+
+char takechar(const char *prompt);
+
+char asciitoc(int c);
+
+
+#endif
